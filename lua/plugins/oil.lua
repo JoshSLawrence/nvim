5f5afc436b839ac541b386c1a76017471181ae12
Josh Lawrence <josh.stephen.lawrence@gmail.com>
2025-03-31 10:21:36 -0400

    feat: mirror oil split keymaps to snacks explorer picker

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -87,8 +87,13 @@ return {
 				keymaps = {
 					["g?"] = { "actions.show_help", mode = "n" },
 					["<CR>"] = "actions.select",
-					["<C-s>"] = { "actions.select", opts = { vertical = true } },
-					["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+
+					-- custom
+					["<C-v>"] = { "actions.select", opts = { vertical = true } },
+					["<C-s>"] = { "actions.select", opts = { horizontal = true } },
+					-- ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+					-- ["<C-h>"] = { "actions.select", opts = { horizontal = true } },
+
 					["<C-t>"] = { "actions.select", opts = { tab = true } },
 					["<C-p>"] = "actions.preview",
 					["<C-c>"] = { "actions.close", mode = "n" },
