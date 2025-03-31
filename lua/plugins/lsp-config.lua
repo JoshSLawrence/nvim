cac6e126c9c6794fc8f2c3cbae31e341324ebf3b
Josh Lawrence <josh.stephen.lawrence@gmail.com>
2025-03-31 10:14:24 -0400

    feat: disable semantic tokens for terraform lsp
    This lsp feature was conflicting with colors in-editor.
    
    I am drawing no value from this feature at the moment -> disabling.

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -82,6 +82,15 @@ return {
 			for server, config in pairs(opts.servers) do
 				lspconfig[server].setup(config)
 			end
+
+			vim.api.nvim_create_autocmd("LspAttach", {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if client.name == "terraformls" then
+						client.server_capabilities.semanticTokensProvider = nil
+					end
+				end,
+			})
 		end,
 	},
 	{
