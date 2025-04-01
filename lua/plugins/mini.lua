93cefe45a2eb3fe16db6b5014e584a49af310aad
Josh Lawrence <josh.stephen.lawrence@gmail.com>
2025-04-01 18:51:24 -0400

    fix: remove comment that broke statusline
    commit 3a4a4bb6 formatted mini.nvim
    
    however, i mistakenly commented out a line which was included in the
    commit
    
    this broke the statusline... woops!

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -4,7 +4,7 @@ return {
 		config = function()
 			require("mini.ai").setup({ n_lines = 500 })
 			require("mini.surround").setup()
-			-- local statusline = require 'mini.statusline'
+			local statusline = require("mini.statusline")
 			statusline.setup({ use_icons = true })
 			statusline.section_location = function()
 				return "%2l:%-2v"
