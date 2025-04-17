2c65e67473608ec6e0acac54b24644b97e915280
Josh Lawrence <josh.stephen.lawrence@gmail.com>
2025-04-17 00:25:14 -0400

    feat: added auto tag
    combined in autopairs lua due to similar function

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,13 +1,29 @@
 return {
-    'windwp/nvim-autopairs',
-    event = 'InsertEnter',
-    -- Optional dependency
-    dependencies = { 'hrsh7th/nvim-cmp' },
-    config = function()
-        require('nvim-autopairs').setup {}
-        -- If you want to automatically add `(` after selecting a function or method
-        local cmp_autopairs = require 'nvim-autopairs.completion.cmp'
-        local cmp = require 'cmp'
-        cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
-    end,
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {
+			enable_close = true,
+			enable_rename = true,
+			enable_close_on_slash = false,
+		},
+		-- Override by filetype:
+		-- per_file_type = {
+		-- 	["html"] = {
+		-- 		enable_close = false,
+		-- 	},
+		-- },
+	},
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		-- Optional dependency
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			-- If you want to automatically add `(` after selecting a function or method
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
 }
