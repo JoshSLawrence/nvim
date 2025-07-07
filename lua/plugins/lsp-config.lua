6cdc9412a95415b98975368845afd31c2fa7febd
Josh Lawrence <josh@joshlawrence.dev>
2025-07-07 19:39:46 -0400

    chore(lsp): added null check for terraform semantic token disable

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -122,7 +122,7 @@ return {
 			vim.api.nvim_create_autocmd("LspAttach", {
 				callback = function(args)
 					local client = vim.lsp.get_client_by_id(args.data.client_id)
-					if client.name == "terraformls" then
+					if client ~= nil and client.name == "terraformls" then
 						client.server_capabilities.semanticTokensProvider = nil
 					end
 				end,
