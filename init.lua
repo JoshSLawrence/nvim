ea7a8719f59e01bbfab652874ef870ef3956553a
Josh Lawrence <josh@joshlawrence.dev>
2025-07-15 12:41:22 -0400

    feat(opts): auto indent, tab, etc is now 2 spaces, down from 4

--- init.lua
+++ init.lua
@@ -34,9 +34,9 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 vim.opt.expandtab = true
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
 
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
